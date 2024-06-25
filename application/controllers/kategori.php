e484194acf060a8514d70ab5449004de0c238dfd
Up <Up@gmail.com>
2024-06-25 17:30:11 +0700

    UAS WEB 2

---
+++ application/controllers/kategori.php
@@ -0,0 +1,56 @@
+<?php
+
+class Kategori extends CI_Controller{
+
+    
+
+    public function pakaian_pria(){
+
+        $data['pakaian_pria'] = $this->model_kategori->data_pakaian_pria()->result();
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('pakaian_pria', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function pakaian_wanita(){
+
+        $data['pakaian_wanita'] = $this->model_kategori->data_pakaian_wanita()->result();
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('pakaian_wanita', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function pakaian_anak_anak(){
+
+        $data['pakaian_anak_anak'] = $this->model_kategori->data_pakaian_anak_anak()->result();
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('pakaian_anak_anak', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function sepatu(){
+
+        $data['sepatu'] = $this->model_kategori->data_sepatu()->result();
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('sepatu', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function about(){
+
+
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('about');
+        $this->load->view('templates/footer');
+    }
+
+}
---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,23 @@
+<div class="container-fluid">
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="true">
+        <div class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></button>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2" ></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="<?php echo base_url('assets/img/slider-admin.jpg') ?>" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- row ux-->
+   
